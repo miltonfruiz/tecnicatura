761f9e1733bf43310d2fbea77f6e0b23a412747d
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-04-22 02:27:01 -0300

    Ejercicio N° 1

---
+++ Programación I/C/Unidad 2/PI - Unidad II - Práctica en C/ej01dosValores.c
@@ -0,0 +1,19 @@
+/* 1- Ingresar dos valores enteros, sumarlos e imprimir esta suma. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int val1, val2, suma;
+    suma = 0;
+
+    printf("Ingrese el primer valor: \n");
+    scanf("%i", &val1);
+    printf("Ingrese el segundo valor: \n");
+    scanf("%i", &val2);
+
+    suma = val1 + val2;
+    printf("La suma es: %i\n", suma);
+
+    system("pause");
+}
