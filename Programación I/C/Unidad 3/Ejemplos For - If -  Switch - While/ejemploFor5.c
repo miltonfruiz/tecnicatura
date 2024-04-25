4bc5631a27ee4dcdd5bbbe6ca7adb95045c6d597
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-04-25 09:17:40 -0300

    Ejemplo 5 de for (bucle infinito)

---
+++ Programación I/C/Unidad 3/Ejemplos For - If -  Switch - While/ejemploFor5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Ejemplo Bucle infinito
+
+int main(){
+    int i;
+    for (i = 1; i <= 100; i--)
+    {
+        printf("%d", i);
+        printf(" - ");
+    }
+
+    return 0;
+
+}
