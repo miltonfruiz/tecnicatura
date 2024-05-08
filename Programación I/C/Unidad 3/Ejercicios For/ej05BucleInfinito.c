9c18a68a12c9eb0f2f27e6a47a28291cff2d3efb
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-05-08 00:23:40 -0300

    Ejercicio N° 5

---
+++ Programación I/C/Unidad 3/Ejercicios For/ej05BucleInfinito.c
@@ -0,0 +1,17 @@
+/* 5. Generar un bucle infinito */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int i;
+
+    for (i = 1; i <= 10; i--)
+    {
+        printf("%i", i);
+        printf(" - ");
+}
+
+
+    system("pause");
+}
