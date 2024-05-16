f0d19a17521bde71ca596fe9f2b2e2daa1900a2f
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-05-16 00:31:38 -0300

    Ejercicio N° 10

---
+++ Programación I/C/Unidad 3/Ejercicios For/ej10DiezVeces.c
@@ -0,0 +1,15 @@
+/* 10. Imprimir por consola 10 veces el carácter @ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int i;
+
+    for ( i = 1; i <= 10; i++)
+    {
+        printf("@\n");
+    }
+    
+    system("pause");
+}
