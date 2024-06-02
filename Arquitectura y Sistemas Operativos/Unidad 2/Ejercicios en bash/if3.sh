a6b2a460cbe9d7916318012e91f8be55936d0341
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-06-02 19:59:52 -0300

    Prueba Scripting 3

---
+++ Arquitectura y Sistemas Operativos/Unidad 2/Ejercicios en bash/if3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for file in $(ls);
+do
+ if [[ -d $file ]];
+ then
+ echo directorio: $file
+ else
+ if [[ -x $file ]];
+ then
+ echo archivo ejecutable: $file
+ else
+ echo archivo no ejecutable: $file
+ fi
+ fi
+done
