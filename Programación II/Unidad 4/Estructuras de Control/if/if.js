63cd1ffb84e994dbaa88330a19e8ed997d7a733c
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-04 11:43:08 -0300

    Estrucuturas de Control: Ejemplo Switch

--- Programación II/Unidad 4/Estructuras de Control/if/if.js
+++ Programación II/Unidad 4/Estructuras de Control/if/if.js
@@ -39,3 +39,36 @@ if (nota < 5) {
   calificacion = "Sobresaliente";
 }
 console.log("He obtenido un", calificacion);
+
+// Ejemplo switch
+
+var nota = 7;
+console.log("He realizado mi examen. Mi resultado es el siguiente:");
+switch (nota) {
+  case 10:
+    calificacion = "Sobresaliente";
+    break;
+  case 9:
+  case 8:
+    calificacion = "Notable";
+    break;
+  case 7:
+  case 6:
+    calificacion = "Bien";
+    break;
+  case 5:
+    calificacion = "Suficiente";
+    break;
+  case 4:
+  case 3:
+  case 2:
+  case 1:
+  case 0:
+    calificacion = "Insuficiente";
+    break;
+  default:
+    //Cualquier otro caso
+    calificacion = "Nota errónea";
+    break;
+}
+console.log("He obtenido un", calificacion);
