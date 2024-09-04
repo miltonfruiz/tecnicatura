0add48faf3e955718e333478b70d037b52aa8aed
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-04 11:32:27 -0300

    Estrucuturas de Control: Ejemplo if/else

--- Programación II/Unidad 4/Estructuras de Control/if/if.js
+++ Programación II/Unidad 4/Estructuras de Control/if/if.js
@@ -6,3 +6,16 @@ console.log("Nota: ", nota);
 if (nota >= 5) {
   console.log("¡Estoy aprobado!");
 }
+
+// Ejemplo if/ else
+var nota = 7;
+console.log("El examen ha resultado:");
+// Condición
+if (nota < 5) {
+  // Acción A (nota es menor que
+  calificacion = "suspendido";
+} else {
+  // Acción B: (nota es mayor o igual que
+  calificacion = "aprobado";
+}
+console.log("Estoy", calificacion);
