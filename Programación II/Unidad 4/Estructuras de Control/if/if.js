26af328796612670b1bfc6255f913b6cd0aa16c0
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-04 11:54:17 -0300

    Estrucuturas de Control: Ejemplo If con || (OR)

--- Programación II/Unidad 4/Estructuras de Control/if/if.js
+++ Programación II/Unidad 4/Estructuras de Control/if/if.js
@@ -74,7 +74,7 @@ switch (nota) {
 console.log("He obtenido un", calificacion);
 
 // Ejemplo If con && (AND)
-var altura = 0;
+/*var altura = 0;
 var edad = 0;
 altura = parseFloat(prompt("Ingrese la altura"));
 edad = parseInt(prompt("Ingrese la edad"));
@@ -82,4 +82,13 @@ if (altura > 1.3 && edad > 14) {
   console.log("Cumple con los requisitos");
 } else {
   console.log("No cumple con los requisitos");
+}*/
+
+// Ejemplo If con ||(OR)
+var color;
+color = prompt("Ingrese el color del auto");
+if (color == "Rojo" || color == "Verde") {
+  console.log("El auto pertenece a la categoría A");
+} else {
+  console.log("El auto pertenece a la categoría B");
 }
