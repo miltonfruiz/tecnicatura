dffd1486e4f93d97470c70666ef3225078f22749
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-09 11:17:44 -0300

    Función con parámetro + calculo

--- Programación II/Unidad 4/Estructuras de Control/if/funciones.js
+++ Programación II/Unidad 4/Estructuras de Control/if/funciones.js
@@ -3,20 +3,24 @@ function saludar() {
   // Contenido de la función
   console.log("Hola, soy una función");
 }
-
 saludar();
-
 for (let i = 0; i < 10; i++) {
   saludar();
 }
 
 //Funcion con parametro
-
 function saludarPorNombre(nombre, edad) {
   console.log("Hola", nombre, "tenes", edad, "años");
 }
-
 let nombreEnviado = "Milton";
 let edadEnviiado = 35;
-
 saludarPorNombre(nombreEnviado, edadEnviiado);
+
+//Funcion con parametro + calculo
+function tabla(numero) {
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+    console.log("ACA", numero + "x" + i + "=" + numero * i);
+  }
+}
+tabla(1);
