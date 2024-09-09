0220acc781b1f7631083cc1d19366c0b4f1da613
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-09 12:30:45 -0300

    Variables: Let y Var

--- Programación II/Unidad 4/Estructuras de Control/if/funciones.js
+++ Programación II/Unidad 4/Estructuras de Control/if/funciones.js
@@ -99,3 +99,15 @@ const saludo2 = function (nombre) {
   var mensaje = "Hola " + nombre;
   return mensaje;
 };
+
+// Variables: Let y Var
+var a = 5;
+var b = 10;
+if (a === 5) {
+  let a = 4; // El alcance es dentro del bloque if
+  var b = 15; // El alcance es global, sobreescribe a 10
+  console.log(a); // 4, por alcance a nivel de bloque
+  console.log(b); // 15, por alcance global
+}
+console.log(a); // 5, por alcance global
+console.log(b); // 15, por alcance global
