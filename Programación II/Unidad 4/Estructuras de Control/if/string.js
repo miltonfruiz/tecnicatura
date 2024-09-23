e1cedaf00e5268d47e68672a5eac6a1b64305cb9
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-23 11:06:25 -0300

    Ejemplo: String |.length y .concat(str1,str2...)

---
+++ Programación II/Unidad 4/Estructuras de Control/if/string.js
@@ -0,0 +1,33 @@
+// Ejemplo: String |.length y .concat(str1,str2...)
+
+// Creamos una variable de texto
+var texto1 = "¡Hola a todos!";
+largo = texto1.length;
+console.log(largo); //14
+// Se puede usar directamente:
+console.log("Hola".length); //4
+
+// Creamos una variable de texto
+var cad = "¡Hola ";
+// Concatenamos con otra
+var saludo = cad.concat("Codo a Codo!");
+// Y en la consola vemos "¡Hola Codo a Codo!"
+console.log(saludo);
+
+//Ejemplo sin concatenar
+var saludo = "Hola";
+var nombre = "Juan";
+var mensaje = saludo + "," + nombre + "!";
+console.log(mensaje);
+
+//Ejemplo concatenado
+var saludo = "Hola";
+var nombre = "Juan";
+var final = "Como estas";
+var mensaje = saludo.concat(", ", nombre, "! ", final, "?"); //Para concatenar se usa coma (,)
+console.log(mensaje);
+
+//Ejemplo concat2
+var hola = "Hola ";
+var concateno = hola.concat("Milton");
+console.log(concateno);
