85f5a55daa226234ca23f5db5f81e227d33f4713
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-23 12:22:51 -0300

    Ejemplos (crear,acceder y mostrar elementos)

---
+++ Programación II/Unidad 4/Estructuras de Control/if/arrays.js
@@ -0,0 +1,12 @@
+//Ejemplos (crear,acceder y mostrar elementos)
+const vector = [1, 3, 5, 8]; //0, 1, 2, 3: cantidad de elementos - 1
+const vectorVacio = []; //Vector vacío
+const vectorDos = new Array("a", "b", "c");
+const vectorTres = new Array(1, 5, 10, 15, 20);
+
+console.log(vector);
+console.log("Vector vacio:", vectorVacio);
+console.log(vectorDos);
+console.log(vectorDos[1]);
+console.log(vectorTres[2]);
+console.log(vectorTres[6]);
