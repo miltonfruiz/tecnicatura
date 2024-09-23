634fc085d2a277d7d8a743513c356b1a9b3c68b2
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-23 12:39:46 -0300

    Array | Métodos (funciones)

--- Programación II/Unidad 4/Estructuras de Control/if/arrays.js
+++ Programación II/Unidad 4/Estructuras de Control/if/arrays.js
@@ -17,3 +17,50 @@ console.log("Elementos del vector 2:");
 for (i = 0; i < vectorDoss.length; i++) {
   console.log(vectorDoss[i]);
 }
+
+//Array | Métodos (funciones)
+
+//Array |Métodos |Push y Pop
+var frutas = ["Banana", "Naranja"];
+console.log(frutas);
+frutas.push("Kiwi", "Pera");
+console.log(frutas);
+
+var frutas = ["Banana", "Naranja"];
+console.log(frutas);
+console.log(frutas.pop());
+console.log(frutas);
+
+//Array | Métodos |Unshift y Shift
+var colores = ["Rojo", "Celeste"];
+console.log(colores);
+colores.unshift("Azul", "Naranja");
+console.log(colores);
+
+var colores = ["Rojo", "Celeste"];
+console.log(colores);
+console.log(colores.shift());
+console.log(colores);
+
+//Ejemplo: Array | Métodos | Concat
+var colores = ["Rojo", "Celeste"];
+console.log(colores);
+var masColores = ["Verde", "Negro"];
+console.log(masColores);
+//Los elementos de masColores se concatenan
+//al final de los elementos de colores:
+var todos = colores.concat(masColores);
+console.log(todos);
+
+//Ejemplo: Array | Métodos | IndexOf y LastIndexOf
+var letras = ["A", "B", "C", "D", "E", "B", "C"];
+//Buscamos de izquierda a derecha
+var posB1 = letras.indexOf("B");
+console.log("La primera 'B' tiene indice", posB1);
+var posB2 = letras.indexOf("B", 2);
+console.log("La segunda 'B' tiene indice", posB2);
+//Buscamos de derecha a izquierda
+var posA = letras.lastIndexOf("A");
+console.log("La última 'A' tiene indice", posA);
+var posB = letras.lastIndexOf("B");
+console.log("La última 'B' tiene indice", posB);
