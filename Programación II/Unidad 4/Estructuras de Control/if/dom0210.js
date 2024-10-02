8943fd67aafbed89b0c8ca59976038b5139a21e5
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-02 11:43:31 -0300

    DOM | .createElement() y .appendChild()

---
+++ Programación II/Unidad 4/Estructuras de Control/if/dom0210.js
@@ -0,0 +1,3 @@
+const btn = document.createElement("button"); //Creamos el boton y lo guardamos en btn
+btn.innerHTML = "Soy un botón!"; // Le ponemos el texto
+document.body.appendChild(btn); // Lo agregamos al <body>
