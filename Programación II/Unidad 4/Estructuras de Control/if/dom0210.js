6394aa585740c3e6286105f93cceafe48297deac
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-02 12:03:26 -0300

    Crear boton y texto

--- Programación II/Unidad 4/Estructuras de Control/if/dom0210.js
+++ Programación II/Unidad 4/Estructuras de Control/if/dom0210.js
@@ -1,3 +1,10 @@
+//Crear boton
 const btn = document.createElement("button"); //Creamos el boton y lo guardamos en btn
 btn.innerHTML = "Soy un botón!"; // Le ponemos el texto
 document.body.appendChild(btn); // Lo agregamos al <body>
+
+//Crear texto
+let nuevoParrafo = document.createElement("p");
+nuevoParrafo.textContent = "Este es un nuevo parrafo";
+let div = document.getElementById("miDiv");
+div.appendChild(nuevoParrafo);
