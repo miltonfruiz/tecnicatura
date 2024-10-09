a04e23db23dfeb28c987484e2ea2bd990ac281f8
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-09 11:16:28 -0300

    Unidad 5 - Ejemplo de python

---
+++ Programación II/Unidad 5/ejemplos.py
@@ -0,0 +1,5 @@
+nombre = 'Maria'
+edad = 18
+nota = 8
+print(f'El nombre del alumno es {nombre} y tiene {edad} años de edad.')
+print(f"{nombre} tiene una nota de {nota}. {'Aprobo' if nota >= 6 else 'Desaprobo'}.")
