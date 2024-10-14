02053a555cd34f3eece80fc6deec78f9b889b99e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-14 11:07:17 -0300

    Cadenas de caracteres | Métodos 2

--- Programación II/Unidad 5/salida.py
+++ Programación II/Unidad 5/salida.py
@@ -26,7 +26,7 @@ print(saludo)
 
 var1 = 3 + 5 # 8 (entero)
 var2 = "3" + "5" # 35 (cadena)
-var3 = 3 + "5" # TypeError
+'''var3 = 3 + "5" # TypeError'''
 var4 = str(3) + "5" # 35 (cadena)
 var5 = 3 + int("5") # 8 (entero)
 
@@ -37,12 +37,22 @@ print(cadena1 > cadena2)
 print(cadena1 == cadena2)
 print(cadena1 < cadena2)
 
-cadena1 = "Hola" 
+cadena1 = "Hola"
 cadena2 = "hola"
-print(cadena1 == cadena2) 
+print(cadena1 == cadena2)
 
 #Cadenas de caracteres |Métodos
-cadena = "Codo a Codo" 
-print(cadena.upper()) # CODO A CODO 
-print(cadena.lower()) # codo a codo 
+cadena = "Codo a Codo"
+print(cadena.upper()) # CODO A CODO
+print(cadena.lower()) # codo a codo
 print(cadena.capitalize()) # Codo a codo
+
+#         012345678901
+cadena = "¡Hola mundo!"
+print(cadena[6:11]) # mundo
+print(cadena[2:12:2]) # oamno
+print(cadena[6:]) # mundo!
+print(cadena[:5]) # ¡Hola
+print(cadena[:]) # ¡Hola mundo!
+print(cadena[::2]) # ¡oamno
+print(cadena[::-1]) # !odnum aloH¡
