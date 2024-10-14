14fa114152dd8138ba56a879bb72da9a1186f257
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-14 10:53:38 -0300

    Cadenas de caracteres

---
+++ Programación II/Unidad 5/salida.py
@@ -0,0 +1,20 @@
+#Cadenas de caracteres
+
+#Definición de cadenas usando comillas dobles 
+dia1 = "Lunes"
+x = "" # x es un string de longitud cero
+# Definición de cadenas usando comillas simples 
+dia2 = 'Martes'
+z = "121" # z contiene dígitos, pero es un string
+
+risa = 'ja'
+carcajada = risa*5 # jajajajaja
+asteriscos = "*"*10 # **********
+
+# Definición de cadenas usando comillas dobles triples: 
+cadena1 = """En Python es posible definir
+cadenas de caracteres utilizando más de una 
+línea de código"""
+# Definición de cadenas usando comillas simples triples: 
+cadena2 = '''Por supuesto, se puede hacer
+lo mismo utilizando comillas simples'''
