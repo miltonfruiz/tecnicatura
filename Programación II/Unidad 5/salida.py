681ba2e6956e13bc7f711cc5479b08f8d340c356
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-14 10:56:24 -0300

    Cadenas de caracteres | Concatenacion

--- Programación II/Unidad 5/salida.py
+++ Programación II/Unidad 5/salida.py
@@ -1,9 +1,9 @@
 #Cadenas de caracteres
 
-#Definición de cadenas usando comillas dobles 
+#Definición de cadenas usando comillas dobles
 dia1 = "Lunes"
 x = "" # x es un string de longitud cero
-# Definición de cadenas usando comillas simples 
+# Definición de cadenas usando comillas simples
 dia2 = 'Martes'
 z = "121" # z contiene dígitos, pero es un string
 
@@ -11,10 +11,22 @@ risa = 'ja'
 carcajada = risa*5 # jajajajaja
 asteriscos = "*"*10 # **********
 
-# Definición de cadenas usando comillas dobles triples: 
+# Definición de cadenas usando comillas dobles triples:
 cadena1 = """En Python es posible definir
-cadenas de caracteres utilizando más de una 
+cadenas de caracteres utilizando más de una
 línea de código"""
-# Definición de cadenas usando comillas simples triples: 
+# Definición de cadenas usando comillas simples triples:
 cadena2 = '''Por supuesto, se puede hacer
 lo mismo utilizando comillas simples'''
+
+#Cadenas de caracteres |Concatenación
+nombre= input("Ingrese su nombre: ")
+saludo= "Hola "+ nombre
+print(saludo)
+
+var1 = 3 + 5 # 8 (entero)
+var2 = "3" + "5" # 35 (cadena)
+var3 = 3 + "5" # TypeError
+var4 = str(3) + "5" # 35 (cadena)
+var5 = 3 + int("5") # 8 (entero)
+
