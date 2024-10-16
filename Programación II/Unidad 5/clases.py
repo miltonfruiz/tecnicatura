247a853c00dd8143e47e9b7f131ef8a93a82ea92
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-16 11:22:42 -0300

    Clases | Ejemplo de uso de clases y objetos

--- Programación II/Unidad 5/clases.py
+++ Programación II/Unidad 5/clases.py
@@ -53,8 +53,7 @@ class Persona():
      self.nombre = nombre
      self.edad = edad
 def identificarse(self): # Método normal
-    print(f"Hola. Soy {self.nombre} y
-tengo {self.edad} años.")
+    print(f'Hola. Soy {self.nombre} y tengo {self.edad} años.')
 persona1 = Persona() # Instanciamos
 persona1.constructor("Juan", 42)
 persona1.identificarse()
@@ -96,3 +95,35 @@ perro1 = Perro("Lassie", "Collie")
 print(perro1.nombre) # Lassie
 print(perro1.raza) # Collie
 del perro1 # Objeto eliminado.
+
+class Alumno: # Creamos la clase
+    nro_alumnos = 0 # Cantidad de legajos existentes
+#Constructor
+    def __init__ (self,nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+        Alumno.nro_alumnos += 1 # Agregamos un legajo
+# Mostrar datos del objeto
+    def __str__ (self):
+        return f"Nombre: {self.nombre} (nota: {self.nota})"
+# Damos de baja el alumno
+    def __del__ (self):
+        Alumno.nro_alumnos -= 1 # Restamos un legajo
+        print("Alumno dado de baja.")
+        print(f"{Alumno.nro_alumnos} legajos restantes.")
+
+def mostrar_estado(self): # ¿está aprobado?
+    print(f"El estado de {self.nombre} es ",end="" )
+    if self.nota <= 4:
+        print("regular")
+    elif self.nota < 9:
+        print("bueno")
+    else:
+        print("excelente")
+# Programa principal
+alumno1 = Alumno("Aldo López", 8)
+alumno2 = Alumno("JuanaMartín", 3)
+print(alumno1) # Nombre: Aldo López (nota: 8)
+print(alumno2) # Nombre: Juana Martín (nota: 3)
+alumno1.mostrar_estado() # El…de Aldo López es bueno
+alumno2.mostrar_estado() # El…Juana Martín es regular
