323a94c10eae6b441cf5cae8fd6df6ad032ada17
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-16 10:54:24 -0300

    Creación de atributos de instancia

--- Programación II/Unidad 5/clases.py
+++ Programación II/Unidad 5/clases.py
@@ -7,3 +7,12 @@ class Persona:
 juan = Persona()
 # Imprimimos un atributo del objeto
 print(juan.piernas) # 2
+
+class Persona:
+    piernas = 2 # Atributo de clase
+# Instanciamos un objeto de la clase Persona
+juan = Persona()
+# creamos un atributo para el objeto
+juan.edad = 34
+# Mostramos el atributo creado
+print(juan.edad) # 34
