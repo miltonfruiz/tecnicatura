20b21bd96d526e933ebaa0a810858654076f82c1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-16 10:59:47 -0300

    __init__

--- Programación II/Unidad 5/clases.py
+++ Programación II/Unidad 5/clases.py
@@ -60,3 +60,16 @@ persona1.constructor("Juan", 42)
 persona1.identificarse()
 persona1.edad = 43 # Modificamos la edad
 persona1.identificarse()
+
+class Persona():
+# Método constructor
+    def __init__ (self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    def identificarse(self): # Método normal
+        print(f"Hola. Soy {self.nombre} y tengo {self.edad} años.")
+# Instanciamos
+persona1 = Persona("Juan", 42)
+persona1.identificarse()
+persona1.edad = 43 # Modificamos la edad
+persona1.identificarse()
