f5175002e368e9f6433765127e03290a896ba86f
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-16 10:53:51 -0300

    Creación de una clase y un objeto

---
+++ Programación II/Unidad 5/clases.py
@@ -0,0 +1,9 @@
+# Definimos la clase Persona
+class Persona:
+# Atributo, presente en todos
+# los objetos que pertenecen a la clase
+    piernas = 2
+# Instanciamos un objeto de la clase Persona
+juan = Persona()
+# Imprimimos un atributo del objeto
+print(juan.piernas) # 2
