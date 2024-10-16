d3db7137f6a01c2894f078cd874a790dd413c5a4
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-10-16 11:01:35 -0300

    Clases | Método str ()

--- Programación II/Unidad 5/clases.py
+++ Programación II/Unidad 5/clases.py
@@ -73,3 +73,15 @@ persona1 = Persona("Juan", 42)
 persona1.identificarse()
 persona1.edad = 43 # Modificamos la edad
 persona1.identificarse()
+
+# Creamos la clase "Alumno":
+class Alumno:
+    def __init__ (self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+    def __str__ (self):
+        return f"La nota de {self.nombre} es {self.nota}"
+alumno1 = Alumno("Pedro", 7)
+print(alumno1) # La nota de Pedro es 7
+alumno1.nota = 10
+print(alumno1) # La nota de Pedro es 10
