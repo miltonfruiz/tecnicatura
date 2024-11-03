d41ebef7e42bc5189ac1fe89e19348a92c8c5d1f
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-11-03 02:29:21 -0300

    Creación archivo forms.py

---
+++ Programación II/Unidad 5/Ejercicios/Ejercicio Evaluatorio - Python/photo_album/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField, SubmitField
+from wtforms.validators import DataRequired
+
+class PhotoForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    image = FileField('Image', validators=[DataRequired()])
+    submit = SubmitField('Submit')
