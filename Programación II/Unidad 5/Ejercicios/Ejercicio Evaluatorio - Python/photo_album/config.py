dd4592b86c647e6ce636a5eda8ea4caa209403c3
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-11-03 05:43:06 -0300

    Actualización 03/11 05:43

--- Programación II/Unidad 5/Ejercicios/Ejercicio Evaluatorio - Python/photo_album/config.py
+++ Programación II/Unidad 5/Ejercicios/Ejercicio Evaluatorio - Python/photo_album/config.py
@@ -3,6 +3,6 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = 'tp_python'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "photo_album.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
