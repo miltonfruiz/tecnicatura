64eaba9086e026ec985c8413038652a39b7bc885
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-12-08 03:28:28 -0300

    Reestructuración de proyecto: Correción de rutas en index.html

--- Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
+++ Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
@@ -4,13 +4,13 @@ from app.models import db
 from app.routes import routes
 import os
 
-app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'app/templates'))
+app = Flask(__name__,
+            template_folder=os.path.join(os.getcwd(), 'app/templates'),
+            static_folder=os.path.join(os.getcwd(), 'app/static'))
+
 app.config.from_object(Config)
-
 db.init_app(app)
-
 app.register_blueprint(routes)
-
 with app.app_context():
     db.create_all()
 
