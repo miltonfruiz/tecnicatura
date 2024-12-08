f0afcc383ecc278a4d8bd1b343133810bc0b9585
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-12-08 02:11:33 -0300

    Reestructuración de proyecto: Correción archivo app.py

--- Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
+++ Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from app.config import Config
 from app.models import db
 from app.routes import routes
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'app/templates'))
 app.config.from_object(Config)
 
 db.init_app(app)
