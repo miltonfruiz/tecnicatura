17a55766556f008b7a8a0297ac36e0e6eb181677
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-12-08 00:53:04 -0300

    Reestructuración de proyecto: Creación de función agregar_curso()

--- Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
+++ Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app.py
@@ -8,6 +8,19 @@ estudiantes = []
 def index():
     return render_template('index.html')
 
+@app.route('/cursos', methods=['POST'])
+def agregar_curso():
+    data = request.json
+    curso_corregido = data.get('nombre', '').capitalize()
+    profesor_corregido = data.get('profesor', '').capitalize()
+    if any(curso['nombre'].lower() == curso_corregido.lower() for curso in cursos):
+        return jsonify({'mensaje': '¡Ese curso ya existe!', 'tipo': 'error'}), 400
+    if not curso_corregido or not profesor_corregido:
+        return jsonify({'mensaje': '¡Valores ingresados incorrectos!', 'tipo': 'error'}), 400
+    nuevo_curso = {'nombre': curso_corregido, 'profesor': profesor_corregido}
+    cursos.append(nuevo_curso)
+    return jsonify({'mensaje': '¡Curso creado correctamente!', 'tipo': 'success', 'curso': nuevo_curso}), 201
+
 @app.route('/api/cursos', methods=['GET', 'POST'])
 def manejar_cursos():
     if request.method == 'POST':
