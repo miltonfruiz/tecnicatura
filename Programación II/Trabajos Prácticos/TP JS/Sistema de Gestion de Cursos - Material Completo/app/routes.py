add64ce782bb9a7a0ad5af97ece2987e36abd2c7
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-12-08 03:58:58 -0300

    Reestructuración de proyecto: Correción ruta para agregar, eliminar, editar estudiante

--- Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app/routes.py
+++ Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, render_template
-from app.models import db, Curso
+from app.models import db, Curso, Estudiante
 
 routes = Blueprint('routes', __name__)
 
@@ -41,3 +41,51 @@ def eliminar_curso(id):
     db.session.delete(curso)
     db.session.commit()
     return jsonify({"mensaje": f"Curso '{curso.nombre}' eliminado correctamente", "tipo": "success"}), 200
+
+@routes.route('/api/estudiantes', methods=['POST'])
+def agregar_estudiante():
+    data = request.get_json()
+    nombre = data.get('nombre')
+    edad = data.get('edad')
+    nota = data.get('nota')
+    curso_id = data.get('curso_id')
+
+    if not nombre or edad <= 0 or nota < 0 or nota > 10:
+        return jsonify({"mensaje": "Datos incorrectos", "tipo": "error"}), 400
+    curso = Curso.query.get(curso_id)
+    if not curso:
+        return jsonify({"mensaje": "Curso no encontrado", "tipo": "error"}), 404
+    estudiante_existente = next(
+        (est for est in curso.estudiantes if est.nombre.lower() == nombre.lower()), None)
+    if estudiante_existente:
+        return jsonify({"mensaje": "El estudiante ya existe", "tipo": "error"}), 400
+    nuevo_estudiante = Estudiante(nombre=nombre, edad=edad, nota=nota)
+    curso.estudiantes.append(nuevo_estudiante)
+    db.session.commit()
+    return jsonify({"mensaje": "Estudiante agregado correctamente", "tipo": "success", "estudiante": nuevo_estudiante.to_dict()}), 201
+
+@routes.route('/api/estudiantes/<int:id>', methods=['DELETE'])
+def eliminar_estudiante(id):
+    estudiante = Estudiante.query.get(id)
+    if not estudiante:
+        return jsonify({"mensaje": "Estudiante no encontrado", "tipo": "error"}), 404
+    db.session.delete(estudiante)
+    db.session.commit()
+    return jsonify({"mensaje": f"Estudiante {estudiante.nombre} eliminado correctamente", "tipo": "success"}), 200
+
+@routes.route('/api/estudiantes/<int:id>', methods=['PUT'])
+def editar_estudiante(id):
+    estudiante = Estudiante.query.get(id)
+    if not estudiante:
+        return jsonify({"mensaje": "Estudiante no encontrado", "tipo": "error"}), 404
+    data = request.get_json()
+    nombre = data.get('nombre')
+    edad = data.get('edad')
+    nota = data.get('nota')
+    if not nombre or edad <= 0 or nota < 0 or nota > 10:
+        return jsonify({"mensaje": "Datos incorrectos", "tipo": "error"}), 400
+    estudiante.nombre = nombre
+    estudiante.edad = edad
+    estudiante.nota = nota
+    db.session.commit()
+    return jsonify({"mensaje": "Estudiante actualizado correctamente", "tipo": "success", "estudiante": estudiante.to_dict()}), 200
