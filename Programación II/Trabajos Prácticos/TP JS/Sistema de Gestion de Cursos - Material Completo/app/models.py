94b95b65ca1c69a37ca21c0027a532ff6f531945
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-12-08 01:58:56 -0300

    Reestructuración de proyecto: Correción archivo models.py

--- Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app/models.py
+++ Programación II/Trabajos Prácticos/TP JS/Sistema de Gestion de Cursos - Material Completo/app/models.py
@@ -3,15 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Curso(db.Model):
-    __tablename__ = 'cursos'
+    __tablename__ = 'curso'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), unique=True, nullable=False)
-    profesor = db.Column(db.String(100), nullable=False)
-
-    def to_dict(self):
-        return {"id": self.id, "nombre": self.nombre, "profesor": self.profesor}
+    nombre = db.Column(db.String(120), nullable=False)
+    profesor = db.Column(db.String(120), nullable=False)
+    estudiantes = db.relationship('Estudiante', backref='curso', lazy=True)
 
 class Estudiante(db.Model):
+    __tablename__ = 'estudiante'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(80), nullable=False)
+    nombre = db.Column(db.String(120), nullable=False)
     curso_id = db.Column(db.Integer, db.ForeignKey('curso.id'), nullable=False)
