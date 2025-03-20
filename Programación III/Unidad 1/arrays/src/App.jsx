d15aa8bdb73cae45a92ea4d27d83be1dd169ccf4
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-03-20 08:48:27 -0300

    Ejercicio N° 1: Crear función y combinar arreglos

--- Programación III/Unidad 1/arrays/src/App.jsx
+++ Programación III/Unidad 1/arrays/src/App.jsx
@@ -6,5 +6,10 @@ export default function App() {
   const name3 = [...name1, ...name2];
   console.log(name3);
 
+  const numbers = [10, 20, 30, 40];
+  function newArray(star, ArraySP, end) {
+    return [star, ...ArraySP, end];
+  }
+  console.log(newArray(1, numbers, 51));
   return <></>;
 }
