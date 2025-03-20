d928e3e4fc219783fc6ed24e1f1b64c28a59a5f1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-03-20 08:49:09 -0300

    Ejercicio N° 1: Crear función y combinar arreglos

--- Programación III/Unidad 1/arrays/src/App.jsx
+++ Programación III/Unidad 1/arrays/src/App.jsx
@@ -1,11 +1,11 @@
-//1. Combinar los dos arreglos
-
 export default function App() {
+  //1. Combinar los dos arreglos
   const name1 = ["Ana", "Luis", "María"];
   const name2 = ["Carlos", "Sofía", "Pedro"];
   const name3 = [...name1, ...name2];
   console.log(name3);
 
+  //2. Crear función y combinar arreglos
   const numbers = [10, 20, 30, 40];
   function newArray(star, ArraySP, end) {
     return [star, ...ArraySP, end];
