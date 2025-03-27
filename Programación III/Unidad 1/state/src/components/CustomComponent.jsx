657b307852e224f23f76c5262a0e8d6c2ee44679
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-03-27 10:19:15 -0300

    Clase 27/03: Creación función onclickHandler()

--- Programación III/Unidad 1/state/src/components/CustomComponent.jsx
+++ Programación III/Unidad 1/state/src/components/CustomComponent.jsx
@@ -3,15 +3,37 @@ import React, { useState } from "react";
 const CustomComponent = () => {
   const [colors, setColors] = useState("black");
   const [stringColor, setStringColor] = useState("Negro");
+  const [counter, setCounter] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
 
   function colorHandler() {
     setColors(colors === "black" ? "red " : "black");
     setStringColor(stringColor == "Negro" ? "Rojo" : "Negro");
   }
+  function addHandler() {
+    setCounter(counter + 1);
+  }
+  function subtractHandler() {
+    setCounter(counter > 0 ? counter - 1 : setCounter);
+  }
+
+  function resetHandler() {
+    setCounter(0);
+  }
+  function onclickHandler() {
+    setIsClicked(!isClicked);
+  }
   return (
     <div>
       <p style={{ color: colors }}>Soy de color: {stringColor}</p>
-      <button onClick={colorHandler}>Rojo</button>
+      <button onClick={colorHandler}>Click</button>
+      <p>Contador: {counter}</p>
+      <button onClick={subtractHandler}>-</button>
+      <button onClick={addHandler}>+</button>
+      <button onClick={resetHandler}>Reiniciar</button>
+      <p>Aca abajo hay un mensaje secreto</p>
+      <button onClick={onclickHandler}>Mostrar mensaje secreto</button>
+      {isClicked && <p>Este es el mensaje secreto!</p>}
     </div>
   );
 };
