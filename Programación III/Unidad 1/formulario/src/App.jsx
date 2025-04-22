003dfb751ff8ed055b56f9835870516e093e8441
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-22 09:29:34 -0300

    Clase 22/04: Configuración de rutas

--- Programación III/Unidad 1/formulario/src/App.jsx
+++ Programación III/Unidad 1/formulario/src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormPage from "./pages/FormPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <div>
-        <FormPage />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/formulario" element={<FormPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Programación III/Unidad 1/formulario/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido!</h1>
+      <button>Registrarse</button>
+    </div>
+  );
+};
+
+export default Home;
