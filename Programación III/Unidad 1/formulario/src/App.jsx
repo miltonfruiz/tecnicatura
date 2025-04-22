35f6a8b27963ba4206f6de07a9e9c1378dea265e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-22 08:59:03 -0300

    Clase 22/04: Configuración de componentes

--- Programación III/Unidad 1/formulario/src/App.jsx
+++ Programación III/Unidad 1/formulario/src/App.jsx
@@ -1,41 +1,10 @@
-import Form from "./components/Form";
-import Validations from "./components/Validations";
-import { useRef, useState } from "react";
+import FormPage from "./pages/FormPage";
 
 function App() {
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
-  const [errores, setErrores] = useState({});
-
-  const manejarEnvio = (FormData) => {
-    const errores = Validations({ datos: FormData });
-
-    if (Object.keys(errores).length > 0) {
-      if (errores.name && nameRef.current) {
-        nameRef.current.focus();
-      } else if (errores.email && emailRef.current) {
-        emailRef.current.focus();
-      } else if (errores.password && passwordRef.current) {
-        passwordRef.current.focus();
-      }
-
-      setErrores(errores);
-    } else {
-      alert("Formulario enviado con éxito");
-      setErrores({});
-    }
-  };
-
   return (
     <>
       <div>
-        <h1>Formuario</h1>
-        <Form
-          onSubmit={manejarEnvio}
-          errores={errores}
-          refs={{ nameRef, emailRef, passwordRef }}
-        />
+        <FormPage />
       </div>
     </>
   );
