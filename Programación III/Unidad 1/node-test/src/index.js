097b8e1ee3d10f4014b7e81794856ac7d58028ee
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-28 09:39:13 -0300

    Clase 28/04: Creación de modelo book

--- Programación III/Unidad 1/node-test/src/index.js
+++ Programación III/Unidad 1/node-test/src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { PORT } from "./config.js";
 import bookRoutes from "../routes/books.routes.js";
+import { sequelize } from "./db.js";
 
 const app = express();
 
-app.listen(PORT);
-app.use(bookRoutes);
-console.log(`Server listening on port ${PORT}`);
+try {
+  app.listen(PORT);
+  app.use(bookRoutes);
+  await sequelize.authenticate();
+  console.log(`Server listening on port ${PORT}`);
+} catch (error) {
+  console.log(`There was an error on initialization`);
+}
---
+++ Programación III/Unidad 1/node-test/src/models/Book.js
@@ -0,0 +1,18 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+export const Book = sequelize.define("book", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
