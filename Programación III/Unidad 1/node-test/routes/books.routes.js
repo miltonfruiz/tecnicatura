e23f118c6ab4cea21f5f4d5adaa07020ed64be8a
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-28 09:26:18 -0300

    Clase 28/04: Instanciando base de datos

--- Programación III/Unidad 1/node-test/routes/books.routes.js
+++ Programación III/Unidad 1/node-test/routes/books.routes.js
@@ -6,5 +6,16 @@ router.get("/books/:id", (req, res) => {
   const { id } = req.params;
   res.send(`Obteniendo libros con id ${id}`);
 });
-
+router.post("/books/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Creando libros con id ${id}`);
+});
+router.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Borrando libros con id ${id}`);
+});
+router.put("/books/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Actualizando libros con id ${id}`);
+});
 export default router;
