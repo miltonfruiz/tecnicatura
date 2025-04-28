3044e03ce4c9c5e0cb25bc6af5684f202ffc2294
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-28 09:44:44 -0300

    Clase 28/04: Creación de modelo book

--- Programación III/Unidad 1/node-test/src/models/Book.js
+++ Programación III/Unidad 1/node-test/src/models/Book.js
@@ -1,18 +1,38 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../db";
 
-export const Book = sequelize.define("book", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+export const Book = sequelize.define(
+  "book",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+    },
+    pageCount: {
+      type: DataTypes.INTEGER,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+    },
+    avaliable: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  author: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  { timestamps: false }
+);
